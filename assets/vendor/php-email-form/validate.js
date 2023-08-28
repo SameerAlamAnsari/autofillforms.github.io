1b58d69b843666b7719d41912879bc3cbdb61b29
Sameer-Ansari-lab <ecommerce.it.go@gmail.com>
2023-08-28 15:46:12 +0530

    changed all http to https

--- assets/vendor/php-email-form/validate.js
+++ assets/vendor/php-email-form/validate.js
@@ -53,7 +53,7 @@
     fetch(action, {
       method: 'POST',
       body: formData,
-      headers: {'X-Requested-With': 'XMLHttpRequest'}
+      headers: {'X-Requested-With': 'XMLhttpsRequest'}
     })
     .then(response => {
       if( response.ok ) {
